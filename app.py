66b06cbafdbe6f529ea7b165bfb657f7818fe242
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2020-09-25 22:49:37 +0100

    changed the way route is imported

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 from flask_restful import Resource, Api, reqparse
-from routes import route_path
+from routes import *
 
 """
 
