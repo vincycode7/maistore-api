2703d012c6300c6b94bdf2bb115bd5988cd0dcc8
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2020-10-11 16:02:05 +0100

    remove jsonify

--- app_config.py
+++ app_config.py
@@ -107,6 +107,7 @@ def create_and_config_app(app, route_path):
     jwt_error_handler(jwt)
     @app.errorhandler(ValidationError)
     def handle_marshmallow_validation(err):
-        return jsonify({"message" : err}), 400
+        print(f"message message message --> {err}")
+        return {"message" : err}, 400
     link_route_path(api=api, route_path=route_path)
     return app, cors, jwt, api
