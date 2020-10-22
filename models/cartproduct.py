63f96f36b18c268bf63d0558ae2531e9f397e066
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2020-10-22 04:18:10 +0100

    changed type for cartsystem too

--- models/cartproduct.py
+++ models/cartproduct.py
@@ -9,7 +9,7 @@ class CartProductModel(db.Model, ModelsHelper):
 
     # columns
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    product_id = db.Column(db.Integer, db.ForeignKey("product.id"), nullable=False)
+    product_id = db.Column(db.String(50), db.ForeignKey("product.id"), nullable=False)
     store_id = db.Column(db.String(50), db.ForeignKey("store.id"), nullable=False)
     cartsystemid = db.Column(db.Integer, db.ForeignKey("cartsystem.id"), nullable=False)
     user_id = db.Column(db.String(50), db.ForeignKey("user.id"), nullable=False)
