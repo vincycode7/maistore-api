331724b2a68730cb70a5068f13df7c88ef7a1d76
Gbenga Vincent Akinwande <akinwandegbengavincent@gmail.com>
2020-10-22 03:55:04 +0100

    changed how app.before_first_request works in run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, create_usr_from_root
 from db import db
 from ma import ma
 
@@ -9,3 +9,4 @@ ma.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+    create_usr_from_root(app=app)
